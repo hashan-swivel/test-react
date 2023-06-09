5a0bcdf5b611dc00281a79ca182e10c554a05bc7
Hashan-Swivel <102583687+hashan-swivel@users.noreply.github.com>
2023-06-09 14:52:07 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const deps = require('./package.json').dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: 'http://0.0.0.0:3000/'
+    publicPath: 'http://0.0.0.0:3003/'
   },
 
   resolve: {
